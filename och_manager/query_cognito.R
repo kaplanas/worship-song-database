cdef6facb454e4ada12dc3f46cc36f584c55f1c5
Ubuntu <ubuntu@ip-10-0-8-137.us-west-2.compute.internal>
2026-04-04 19:22:15 +0000

    handle congregations with no attributes

--- och_manager/query_cognito.R
+++ och_manager/query_cognito.R
@@ -2,34 +2,42 @@
 get.users = function(cognito.session, user.pool.id, date.placeholder,
                      current.user, pagination.token = NULL) {
   results = cognito.session$list_users(UserPoolId = user.pool.id,
-                                       AttributesToGet = list("name",
-                                                              "custom:city",
-                                                              "custom:state",
-                                                              "custom:size",
-                                                              "custom:praise",
-                                                              "custom:women",
-                                                              "custom:share"),
                                        PaginationToken = pagination.token)
+  attributes.of.interest = c("name", "custom:city", "custom:state",
+                             "custom:size", "custom:praise", "custom:women",
+                             "custom:share")
   df = map_dfr(
     results$Users,
     function(user) {
       temp.df = data.frame(congregation = user$Username)
       for(attribute in user$Attributes) {
-        col.name = gsub(".*:", "", attribute$Name)
-        if(attribute$Name %in% c("custom:share")) {
-          temp.df[[col.name]] = as.logical(as.numeric(attribute$Value))
-        } else if(attribute$Name %in% c("custom:praise", "custom:women")) {
-          if(attribute$Value == date.placeholder) {
-            temp.df[[col.name]] = NA
+        if(attribute$Name %in% attributes.of.interest) {
+          col.name = gsub(".*:", "", attribute$Name)
+          if(attribute$Name %in% c("custom:share")) {
+              temp.df[[col.name]] = as.logical(as.numeric(attribute$Value))
+          } else if(attribute$Name %in% c("custom:praise", "custom:women")) {
+            if(attribute$Value == date.placeholder) {
+              temp.df[[col.name]] = NA
+            } else {
+              temp.df[[col.name]] = ymd(attribute$Value)
+            }
           } else {
-            temp.df[[col.name]] = ymd(attribute$Value)
+            temp.df[[col.name]] = attribute$Value
           }
-        } else {
-          temp.df[[col.name]] = attribute$Value
         }
       }
-      if(!("city" %in% colnames(temp.df))) {
-        temp.df$city = NA_character_
+      if(ncol(temp.df) == 1) {
+        temp.df$name = NA_character_
+      }
+      for(a in c("name", "city", "state", "size")) {
+        if(!(a %in% colnames(temp.df))) {
+          temp.df[[a]] = NA_character_
+        }
+      }
+      for(a in c("praise", "women", "share")) {
+        if(!(a %in% colnames(temp.df))) {
+          temp.df[[a]] = NA
+        }
       }
       temp.df %>%
         mutate(congregation.label = gsub("Church( of Christ)?( (at|in))?", "",
